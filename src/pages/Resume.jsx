8209aa27c628b211cfece9ba45c93d324fd2b4e1
Aman Raj <113578582+huamanraj@users.noreply.github.com>
2024-09-10 02:42:05 +0530

    resume update 8 24

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -12,12 +12,12 @@ function Resume() {
             
         </div>
       <a 
-        href="https://drive.google.com/file/d/1ZrJmtpcBdMbPMBY6sn7XmUh6sHg_eG4m/view?usp=sharing" 
+        href="https://drive.google.com/file/d/1MumjuEau0AlbneZgVVORpXtKJ8UV95WB/view?usp=sharing" 
          target='_blank' rel="noopener noreferrer"
         className="inline-flex items-center bg-md-purple text-md-bg px-6 py-2 rounded hover:bg-md-blue transition-colors duration-300"
       >
         <FaDownload className="mr-2" />
-        Download Full Resume
+        View & Download Resume
       </a>
     </div>
   );
