850a26393577244b9b0ee4d2bd2a4a2fdbaec5fa
aman-raj <amanraj12.ar@gmail.com>
2024-12-03 16:49:13 +0530

    pdf resume added

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,23 +1,27 @@
-// pages/Resume.jsx
 import { FaDownload } from 'react-icons/fa';
 import React from 'react';
-
+import resume from '/Resume.pdf'
 function Resume() {
-
-
-
   return (
     <div className="max-w-3xl mx-auto">
-         <div>
-            
-        </div>
+      {/* Embed the PDF */}
+      <div className="my-2">
+        <iframe 
+          src={`${resume}#toolbar=0&view=FitH`} 
+          title="Aman Raj's Resume"
+          className="w-full sm:h-[720px] h-[500px] border rounded-lg"
+           loading="lazy"
+        ></iframe>
+      </div>
+
+      {/* Download Button */}
       <a 
-        href="https://drive.google.com/file/d/1MumjuEau0AlbneZgVVORpXtKJ8UV95WB/view?usp=sharing" 
-         target='_blank' rel="noopener noreferrer"
+        href={resume}
+        download="Aman_Raj_Resume.pdf"
         className="inline-flex items-center bg-md-purple text-md-bg px-6 py-2 rounded hover:bg-md-blue transition-colors duration-300"
       >
         <FaDownload className="mr-2" />
-        View & Download Resume
+        Download Resume
       </a>
     </div>
   );
